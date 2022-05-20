a29859b34e3c83414ee71dcbb25d072482d0f327
Karlis Ivansons <Karlis@Ivansons.com>
2022-05-20 12:47:49 -0400

    Added netmiko_hw01.py

--- class1/netmiko_hw02.py
+++ class1/netmiko_hw02.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 """
-1. In the lab environment use Netmiko to connect to one of the Cisco NX-OS devices. 
-You can find the IP addresses and username/passwords of the Cisco devices in the 'Lab Environment' email
- or alternatively in the ~/.netmiko.yml file.
-  Simply print the router prompt back from this device to verify you are connecting to the device properly.
+2. Add a second NX-OS device to your first exercise.
+Make sure you are using dictionaries to represent the two NX-OS devices.
+Additionally, use a for-loop to accomplish the Netmiko connection creation.
+Once again print the prompt back from the devices that you connected to.
 """
 from netmiko import ConnectHandler
 from getpass import getpass
 
 nx_password = getpass()
+
 nx01 = {
     "host": "nxos1.lasthop.io",
     "username": "pyclass",
@@ -16,5 +17,13 @@ nx01 = {
     "device_type": "cisco_nxos",
 }
 
-net_connect = ConnectHandler(**nx01)
-print(net_connect.find_prompt())
+nx02 = {
+    "host": "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": nx_password,
+    "device_type": "cisco_nxos",
+}
+devices = [nx01, nx02]
+for device in devices:
+    connection = ConnectHandler(device)
+    print(connection.find_prompt())
