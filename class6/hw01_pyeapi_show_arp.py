d6eba9806ebcdf92bbc69366c2e3bb4f97920231
Karlis Ivansons <Karlis@Ivansons.com>
2022-07-01 12:02:36 -0400

    Removed arp_output data comment

--- class6/hw01_pyeapi_show_arp.py
+++ class6/hw01_pyeapi_show_arp.py
@@ -21,43 +21,6 @@ device = pyeapi.client.Node(connection)
 # Run show ip arp and store output
 arp_output = device.enable("show ip arp")
 
-"""
-[{'command': 'show ip arp',
-  'encoding': 'json',
-  'result': {'dynamicEntries': 7,
-             'ipV4Neighbors': [{'address': '10.220.88.1',
-                                'age': 0,
-                                'hwAddress': '0024.c4e9.48ae',
-                                'interface': 'Vlan1, Ethernet1'},
-                               {'address': '10.220.88.23',
-                                'age': 0,
-                                'hwAddress': '502f.a8b1.6900',
-                                'interface': 'Vlan1, not learned'},
-                               {'address': '10.220.88.28',
-                                'age': 0,
-                                'hwAddress': '00aa.fc05.b513',
-                                'interface': 'Vlan1, not learned'},
-                               {'address': '10.220.88.29',
-                                'age': 0,
-                                'hwAddress': '00af.fc9a.e49e',
-                                'interface': 'Vlan1, not learned'},
-                               {'address': '10.220.88.31',
-                                'age': 0,
-                                'hwAddress': '00ac.fc59.97f2',
-                                'interface': 'Vlan1, not learned'},
-                               {'address': '10.220.88.37',
-                                'age': 0,
-                                'hwAddress': '0001.00ff.0001',
-                                'interface': 'Vlan1, not learned'},
-                               {'address': '10.220.88.38',
-                                'age': 0,
-                                'hwAddress': '0002.00ff.0001',
-                                'interface': 'Vlan1, not learned'}],
-             'notLearnedEntries': 6,
-             'staticEntries': 0,
-             'totalEntries': 7}}]
-
-"""
 # Unpack arp table from json data
 arp_table = arp_output[0]["result"]["ipV4Neighbors"]
 
