742f53b99a3146f97c15c5f622b502b756e71b26
Karlis Ivansons <Karlis@Ivansons.com>
2022-07-11 15:47:10 -0400

    Added route printing loop

--- class6/hw03_show_ip_route.py
+++ class6/hw03_show_ip_route.py
@@ -28,3 +28,41 @@ node = pyeapi.client.Node(connection)
 output = node.enable("show ip route")
 
 pprint(output)
+
+"""
+example output data structure
+[{'command': 'show ip route',
+  'encoding': 'json',
+  'result': {'vrfs': {'default': {'allRoutesProgrammedHardware': True,
+                                  'allRoutesProgrammedKernel': True,
+                                  'defaultRouteState': 'reachable',
+                                  'routes': {'0.0.0.0/0': {'directlyConnected': False,
+                                                           'hardwareProgrammed': True,
+                                                           'kernelProgrammed': True,
+                                                           'metric': 0,
+                                                           'preference': 1,
+                                                           'routeAction': 'forward',
+                                                           'routeType': 'static',
+                                                           'vias': [{'interface': 'Vlan1',
+                                                                     'nexthopAddr': '10.220.88.1'}]},
+                                             '10.220.88.0/24': {'directlyConnected': True,
+                                                                'hardwareProgrammed': True,
+                                                                'kernelProgrammed': True,
+                                                                'routeAction': 'forward',
+                                                                'routeType': 'connected',
+                                                                'vias': [{'interface': 'Vlan1'}]}},
+                                  'routingDisabled': False}}}}]
+
+"""
+# Unpack default vrf routes dictionary from api output
+routes = output[0]["result"]["vrfs"]["default"]["routes"]
+
+# Pull route prefix,type, and nexthop(if applicable) from route
+for key,values in routes.items():
+    route_prefix = key
+    route_type = values["routeType"]
+    if route_type.lower() == "static":
+        route_nexthop_addr = values["vias"][0]["nexthopAddr"]
+    else:
+        route_nexthop_addr = None
+    print(f"{route_prefix:20} {route_type:20} {route_nexthop_addr:20}")
