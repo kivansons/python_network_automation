04d5ecb2cb2ad949b8d0badbc0a8f73655430fd3
Karlis Ivansons <Karlis@Ivansons.com>
2022-07-17 18:42:34 -0400

    Added 7b solution

--- class7/hw07_nxapi_xml.py
+++ class7/hw07_nxapi_xml.py
@@ -5,9 +5,6 @@ The api_format should be "xml" and the transport should be "https" (port 8443). 
 Send the "show interface Ethernet1/1" command to the device, parse the output, and print out the following information:
 
 Interface: Ethernet1/1; State: up; MTU: 1500
-
-
-
 7b. Run the following two show commands on the nxos1 device using a single method and passing in a list of commands: "show system uptime" and "show system resources". Print the XML output from these two commands.
 
 
@@ -42,3 +39,26 @@ print("\n\n")
 print(f"Formatted Interface Information")
 print("-" * 80)
 print(f"Interface: {interface.text}; State: {state.text}; MTU: {mtu.text}")
+"""
+7b. Run the following two show commands on the nxos1 device using a single method and passing in a list of commands:
+"show system uptime" and "show system resources".
+Print the XML output from these two commands.
+"""
+commands = [
+    "show system uptime",
+    "show system resources"
+]
+
+output_list = device.show_list(commands)
+
+for command,result in zip(commands,output_list):
+    print("\n\n")
+    print(f'XML result from "{command}')
+    print("-" * 80)
+    pprint(etree.tostring(result).decode())
+
+"""
+7c. Using the nxapi_plumbing config_list() method,
+configure two loopbacks on nxos1 including interface descriptions.
+Pick random loopback interface numbers between 100 and 199.
+"""
