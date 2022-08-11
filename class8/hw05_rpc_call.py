aa81f7977cc8ef5fc33f03d3e041802edbcb5c09
Karlis Ivansons <Karlis@Ivansons.com>
2022-08-11 10:40:55 -0400

    commit hw05 for pylab testing

---
+++ class8/hw05_rpc_call.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+
+5a. Connect to the srx2 device. Using an RPC call, gather and pretty-print the "show version" information.
+Recall that you can retrieve RPC method name by running "show version | display xml rpc" argument.
+Also don't forget to convert the hyphens to underscores. Your output should match the following:
+
+<software-information>
+<host-name>srx2</host-name>
+<product-model>srx110h2-va</product-model>
+<product-name>srx110h2-va</product-name>
+<jsr/>
+<package-information>
+<name>junos</name>
+<comment>JUNOS Software Release [12.1X46-D35.1]</comment>
+</package-information>
+</software-information>
+
+
+5b. Using a direct RPC call, gather the output of "show interfaces terse". Print the output to standard out.
+
+5c. Modify the previous task to capture "show interface terse", but this time only for "fe-0/0/7".
+Print the output to standard out. Use normalize=True in the RPC method call to make the output more readable.
+You will also need to add pretty_print=True to the etree.tostring() call.
+Consequently, your code should be similar to the following:
+
+xml_out = dev.rpc.get_interface_information(interface_name="fe-0/0/7", terse=True, normalize=True)
+print(etree.tostring(xml_out, pretty_print=True, encoding="unicode"))
+"""
+from jnpr.junos import Device
+from lxml import etree
+from getpass import getpass
+from jnpr_devices import srx2
+
+srx2_device = Device(**srx2)
+srx2_device.open()
+
+# show interfaces terse | display xml rpc
+# <get-interface-information>
+xml_out = srx2_device.rpc.get_interface_information()
+print(xml_out)
