2eef7c1ce26a9c96d12c8e04d37a8f8acc3b243a
Karlis Ivansons <Karlis@Ivansons.com>
2022-08-31 12:05:01 -0400

    initial commit of hw03 for testing

---
+++ class9/hw03_config_merge.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+3a. Using your existing functions and the my_devices.py file, create a NAPALM connection to both cisco3 and arista1.
+
+3b. Create two new text files `arista1.lasthop.io-loopbacks` and `cisco3.lasthop.io-loopbacks`. 
+In each of these files, create two new loopback interfaces with a description. 
+Your files should be similar to the following:
+
+interface loopback100
+  description loopback100
+!
+interface loopback101
+  description loopback101
+
+
+For both cisco3 and arista1, use the load_merge_candidate() method to stage the candidate configuration.
+In other words, use load_merge_candidate() and your loopback configuration file to stage a configuration change.
+Use the NAPALM compare_config() method to print out the pending differences 
+(i.e. the differences between the running configuration and the candidate configuration).
+
+3c. Commit the pending changes to each device, and check the diff once again (after the commit_config).
+"""
+from my_devices import net_devices
+from my_functions import build_napalm_connection
+
+def main():
+    
+    # Create napalm connections
+    net_connections = []
+    for device in net_devices.values():
+        net_connect = build_napalm_connection(device)
+        net_connections.append(net_connect)
+
+    for connection in net_connections:
+        print("\n")
+        print("-" * 80)
+        print(f"Loading config candidiate for {connection.hostname}")
+        connection.load_merge_candiate(filename=f"{connection.hostname}-loopbacks")
+        diff = connection.compare_config()
+        print("Config diff is:")
+        print("-" * 80)
+        print(diff)
+
+
+
+    return None
+
+if __name__ == "__main__":
+    main()
